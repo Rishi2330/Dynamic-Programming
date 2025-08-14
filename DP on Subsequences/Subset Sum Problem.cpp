bd39030f7c0a43184a77e5ea9c29d88058a24b25
Rishi Singh <141866841+Rishi2330@users.noreply.github.com>
2025-08-15 00:15:05 +0530

    Subset Sum Problem.cpp

---
+++ DP on Subsequences/Subset Sum Problem.cpp
@@ -0,0 +1,61 @@
+// Recursion
+class Solution {
+  public:
+    bool f(int i, int target, vector<int>& arr){
+        if(terget == 0) return true;
+        if(i == 0) arr[0] == target;
+        bool take = false;
+        if(arr[i] <= target) take = f(i-1, target-arr[i], arr);
+        bool notTake = f(i-1, target, arr);
+        return take or notTake;
+    }
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        return f(0, sum, arr);
+    }
+};
+
+// Memoization
+class Solution {
+  public:
+    bool f(int i, int target, vector<int>& arr, vector<vector<int>>& dp){
+        if(target == 0) return true;
+        if(i == 0) return (arr[0] == target);
+        if(dp[i][target] != -1) return dp[i][target];
+        bool take = false;
+        if(arr[i] <= target) take = f(i-1, target-arr[i], arr, dp);
+        bool notTake = f(i-1, target, arr, dp);
+        return dp[i][target] = take or notTake;
+    }
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(sum+1, -1));
+        return f(n-1, sum, arr, dp);
+    }
+};
+
+// Tabulation
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        int n = arr.size();
+        vector<vector<bool>> dp(n, vector<bool>(sum+1, false));
+        for(int i=0; i<n; i++){
+            dp[i][0] = true;
+        }
+        if(arr[0] <= sum) dp[0][arr[0]] = true;
+        for(int i = 1; i < n; i++) {
+            for(int target = 1; target <= sum; target++) {
+                bool take = false;
+                if(arr[i] <= target) take = dp[i - 1][target - arr[i]];
+                bool notTake = dp[i - 1][target];
+                dp[i][target] = take || notTake;
+            }
+        }
+        return dp[n - 1][sum];
+    }
+};
+
+// Space Optimization
