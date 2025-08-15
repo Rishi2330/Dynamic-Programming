24d42e4d875e9cd411bfbe563b738b3a9c62999f
Rishi Singh <141866841+Rishi2330@users.noreply.github.com>
2025-08-15 22:32:32 +0530

    Subset Sum Problem.cpp

--- DP on Subsequences/Subset Sum Problem.cpp
+++ DP on Subsequences/Subset Sum Problem.cpp
@@ -59,3 +59,19 @@ class Solution {
 };
 
 // Space Optimization
+    bool f(int k, vector<int>& arr){
+        int n = arr.size();
+        vector<bool> prev(k+1, false), curr(k+1, false);
+        prev[0] = curr[0] = true;
+        if(arr[0] <= k) prev[arr[0]] = true;
+        for(int i=1; i<n; i++){
+            for(int target = 1; target<=k; target++){
+                bool notTake = prev[target];
+                bool take = false;
+                if(arr[i] <= target) take = prev[target-arr[i]];
+                curr[target] = take | notTake;
+            }
+            prev = curr;
+        }
+        return prev[k];
+    }
